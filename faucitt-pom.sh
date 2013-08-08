e1675803a792e09c61357ccdf5080dd6b0540c3b
Dan <themonkey1415@mcdynamite.net76.net>
2013-08-08 13:22:29 -0700

    Update and rename push-pom.sh to faucitt-pom.sh

---
+++ faucitt-pom.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git checkout master
+git add faucitt/pom.xml
+git commit -m "faucitt/pom.xml"
+git push
--- push-pom.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git checkout master
-git add pom.xml
-git commit -m "pom.xml"
-git push
